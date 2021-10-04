1a8e484b0b4d2bc4f2c18ac64e952b44d3b0a4fd
Iskandar <iskandjuma@gmail.com>
2021-10-04 23:01:12 +0300

    Добавляет исправления от наставника

--- gulpfile.js
+++ gulpfile.js
@@ -95,5 +95,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "webp", "html"));
 gulp.task("start", gulp.series("build", "server"));
