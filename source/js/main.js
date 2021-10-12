d45b25c8b3f69debc64056f93b22a0a30e01a598
Iskandar <iskandjuma@gmail.com>
2021-10-12 22:29:32 +0300

    добавляет проверку на элементы js

--- source/js/main.js
+++ source/js/main.js
@@ -4,17 +4,19 @@
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
 
-  if (navMain.classList.contains('main-nav--nojs')) {
+  if (navMain) {
     navMain.classList.remove('main-nav--nojs');
   }
 
-  navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-    }
-  });
+  if (navToggle) {
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+      } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
+    });
+  }
 })();
